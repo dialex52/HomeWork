5e961c099c4160c2b64c615a1dabecec91461c98
Alexey Bobrov <dialex52@yandex.ru>
2021-03-10 15:00:05 +0300

    task_1,task_2,task_3, task_4 completed

---
+++ Lesson_4/task_2,3.py
@@ -0,0 +1,29 @@
+import requests
+import re
+from decimal import *
+import datetime
+
+
+def get_currency_rate(currency):
+    global date
+    response = requests.get('http://www.cbr.ru/scripts/XML_daily.asp')
+    resp_decoding = str(response.content.decode(errors='replace'))
+    separators = '<', '>', '"'
+    sep_string = '|'.join(map(re.escape, separators))
+    sep_list = re.split(sep_string, resp_decoding)
+    if currency.upper() in sep_list:
+        for i in range(len(sep_list)):
+            if sep_list[i] == 'ValCurs Date=':
+                inp_date = (sep_list[i + 1]).split('.')
+                date = datetime.date(int(inp_date[2]), int(inp_date[1]), int(inp_date[0]))
+
+            if currency.upper() == sep_list[i]:
+                rate = round(Decimal(sep_list[i + 12].replace(',', '.')) / Decimal(sep_list[i + 4]), 2)
+                print('1 {} : {} RUB, {}'.format(currency.upper(), rate, date))
+    else:
+        print(None)
+
+
+# inp_currency = (input('Введите валюту, курс котрый вы хотите узнать: ')).upper()
+#
+# get_currency_rate(inp_currency)
---
+++ Lesson_4/task_4.py
@@ -0,0 +1,4 @@
+from utils import get_currency_rate
+
+get_currency_rate('eur')
+get_currency_rate('fht')
